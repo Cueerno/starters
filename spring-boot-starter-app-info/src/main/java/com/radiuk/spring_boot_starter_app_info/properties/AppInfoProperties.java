5262493f1a74662fcb17db9165b97cc377b19290
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-03 14:07:09 +0300

    feat: add default values for properties

--- spring-boot-starter-app-info/src/main/java/com/radiuk/spring_boot_starter_app_info/properties/AppInfoProperties.java
+++ spring-boot-starter-app-info/src/main/java/com/radiuk/spring_boot_starter_app_info/properties/AppInfoProperties.java
@@ -1,11 +1,14 @@
 package com.radiuk.spring_boot_starter_app_info.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "app.info")
-public record AppInfoProperties(
-        boolean enabled,
-        String projectName,
-        String version
-) {
+public class AppInfoProperties {
+    private boolean enabled = true;
+    private String projectName = "Unknown";
+    private String version = "0.0.1";
 }

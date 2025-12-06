6f9246ddf0b6dc42174f5af0e362c04ece08a0f3
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-06 17:21:54 +0300

    feat: add initialization log

--- spring-boot-starter-app-info/src/main/java/com/radiuk/spring_boot_starter_app_info/auto_config/AppInfoAutoConfiguration.java
+++ spring-boot-starter-app-info/src/main/java/com/radiuk/spring_boot_starter_app_info/auto_config/AppInfoAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.radiuk.spring_boot_starter_app_info.controller.AppInfoController;
 import com.radiuk.spring_boot_starter_app_info.properties.AppInfoProperties;
 import com.radiuk.spring_boot_starter_app_info.service.AppInfoService;
 import com.radiuk.spring_boot_starter_app_info.service.impl.AppInfoServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -13,10 +14,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
+@Slf4j
 @AutoConfiguration
 @EnableConfigurationProperties(AppInfoProperties.class)
 @ConditionalOnProperty(value = "app.info.enabled", havingValue = "true", matchIfMissing = true)
 public class AppInfoAutoConfiguration {
+
+    public AppInfoAutoConfiguration() {
+        log.info(">>> AppInfoStarter initialized <<<");
+    }
 
     @Bean
     @ConditionalOnMissingBean
